f1b599e327d44163a0c02f4de9a354ad34bbc979
Firiyuu <stormmuhokushikiro@yahoo.com>
2019-05-14 21:05:55 +0800

    Fixed Deletion Bug

--- so.py
+++ so.py
@@ -191,8 +191,10 @@ class window(QtWidgets.QMainWindow):
         item = sender.text()
         item = item.split('-')[0]
         item = item.strip()
+        print(str(item) + '<----')
         item, quantity = search_request_delete(item)
         print(str(quantity))
+
         if int(quantity) == 1:
            delete_request(item)
         else:
@@ -205,7 +207,7 @@ class window(QtWidgets.QMainWindow):
         total, items = fetch_items()
         for item in items:
             value = str(item[2])
-            item = str(item[0]) + ' PHP' + str(item[1]) + ' - ' + value  +'x'
+            item = str(item[0]) + ' - PHP' + str(item[1]) + ' - ' + value  +'x'
             text+=item +'\n'
         text = text + '\n' + 'Total: ' + str(total)
 
@@ -266,7 +268,7 @@ class window(QtWidgets.QMainWindow):
                 value = str(item[2])
                 if int(value) == 0:
                     value = ''
-                item = str(item[0]) + ' PHP' + str(item[1]) + ' - ' + value  +'x'
+                item = str(item[0]) + ' - PHP' + str(item[1]) + ' - ' + value  +'x'
                 self.b3 = QtWidgets.QPushButton(item)
                 self.b3.setSizePolicy(
                 QtWidgets.QSizePolicy.Preferred,
